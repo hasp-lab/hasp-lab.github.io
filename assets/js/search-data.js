ef627265b1d5de3136e2c8d125a8f94e57e62783
jsuac <jsu.academia@gmail.com>
2026-02-02 15:10:56 +0000

    Deploying to gh-pages from @ hasp-lab/hasp-lab.github.io@0d6586bd1798b2e9c60e1b9f367789e32e0b905c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,9 +51,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-ai-ethics-and-online-harms",
+          title: 'AI Ethics and Online Harms',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
             },},{id: "projects-project-3-with-very-long-name",
